69ae778535c9b8abfbebd3bc367d9dd2dde85100
Myung Choi <myungchoi@gmail.com>
2023-06-29 03:42:39 -0400

    fixed for HikariCP configuration

--- src/main/java/edu/gatech/chai/fhironfhirbase/database/DatabaseConfiguration.java
+++ src/main/java/edu/gatech/chai/fhironfhirbase/database/DatabaseConfiguration.java
@@ -7,7 +7,7 @@ import javax.sql.DataSource;
 public interface DatabaseConfiguration {
 	public void setSqlRenderTargetDialect(String targetDialect);
 	public String getSqlRenderTargetDialect();
-	public javax.sql.DataSource getDataSource();
+	public DataSource getDataSource();
 	public void setDataSource(DataSource dataSource);
 	public Connection getConnection();
 	public void setConnection(Connection connection);
